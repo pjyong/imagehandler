0edec4977e8c2f10c749d2e0aced0270cecf59dd
june <827397687@qq.com>
2016-12-13 00:06:10 +0800

    init

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+var kvStorage map[string]string
+var kVStoreMutex sync.RWMutex
+
+func main() {
+	kvStorage = make(map[string]string)
+	kVStoreMutex = sync.RWMutex{}
+	// 创建key-value相关api
+	http.HandleFunc("/get", get)
+	http.HandleFunc("/set", set)
+	http.HandleFunc("/remove", remove)
+	http.HandleFunc("/list", list)
+	http.ListenAndServe(":3000", nil)
+}
+
+func get(w http.ResponseWriter, r *http.Request) {
+	// 需要检查是不是Get请求
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Error:非法请求")
+		return
+	}
+	values, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		w.WriteHeader(w, http.StatusBadRequest)
+		fmt.Fprint(w, "Error:", err)
+		return
+	}
+	if len(values.Get("key")) == 0 {
+		w.WriteHeader(w, http.StatusBadRequest)
+		fmt.Fprint(w, "Error:参数错误")
+		return
+	}
+	// 设置锁
+	kVStoreMutex.RLock()
+	value := kvStorage[string(values.Get("key"))]
+	kVStoreMutex.RUnlock()
+	fmt.Fprint(w, value)
+}
+
+func set(w http.ResponseWriter, r *http.Request) {
+}
+
+func remove(w http.ResponseWriter, r *http.Request) {
+}
+
+func list(w http.ResponseWriter, r *http.Request) {
+}
